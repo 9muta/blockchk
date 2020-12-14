e2db552fa218d30f7c58a50341e2e068036db862
hiro <hiro@MacBook-Pro.local>
2020-12-14 17:24:19 +0900

    modify commit

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,4 @@
 set :job_template, "$(which zsh) -l -c ':job'"
-set :output , "/Users/hiro/blockchk/config/cron.log"
 
 every 1.minute do
   command ". ~/.zshrc && cd ~/blockchk && bundle exec ./chkBlockHeight.rb"
